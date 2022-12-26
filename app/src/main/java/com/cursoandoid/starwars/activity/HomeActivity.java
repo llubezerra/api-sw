7217b92e102def444b8f0d746254afc697a1f9eb
Luana Bezerra <lu.oliveira_1994@hotmail.com>
2022-12-26 14:00:26 -0300

    criação dos botões de paginação

--- app/src/main/java/com/cursoandoid/starwars/activity/HomeActivity.java
+++ app/src/main/java/com/cursoandoid/starwars/activity/HomeActivity.java
@@ -9,7 +9,6 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
